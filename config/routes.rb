bb4dcf613349d60c2a6635ba1f3410f7939a6920
Manzeel Uprety <mnzel.uprety@gmail.com>
2020-04-06 19:02:25 +0545

    add name column & added signUp/update params to reg controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
 root to: 'pages#index' # call the index method from the pages_controller.rb
 end
+
+
